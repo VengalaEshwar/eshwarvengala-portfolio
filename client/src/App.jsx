c2f77d15b6d027ba0b67f2b8f786473711b7d6ca
Eshwar <eshwarvengala30@gmail.com>
2025-06-09 10:30:19 +0530

    add feature visit count

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 import MainLayout from './layouts/MainLayout'
-
+import { db, doc, getDoc, setDoc, updateDoc } from './utils/firebase';
 function App() {
-  const [count, setCount] = useState(0)
+    useEffect(() => {
+    const checkAndUpdateVisit = async () => {
+      const isVisited = localStorage.getItem('isVisited');
 
+      if (!isVisited) {
+        const visitRef = doc(db, 'analytics', 'visitCount');
+        const snap = await getDoc(visitRef);
+
+        if (snap.exists()) {
+          await updateDoc(visitRef, {
+            count: snap.data().count + 1
+          });
+        } else {
+          await setDoc(visitRef, { count: 1 });
+        }
+
+        localStorage.setItem('isVisited', 'true');
+      }
+    };
+
+    checkAndUpdateVisit();
+  }, []);
   return (
     <MainLayout/>
   )
